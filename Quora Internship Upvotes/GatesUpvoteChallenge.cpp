98d108bdbb5b355710d9b41564aec32256cd5645
Ag8833 <ag8833@aol.com>
2015-09-30 22:18:50 -0700

    Quora Upvote Challenge Fixed
    Fixed adding code to deal with the upvotes in the correct method.

--- Quora Internship Upvotes/GatesUpvoteChallenge.cpp
+++ Quora Internship Upvotes/GatesUpvoteChallenge.cpp
@@ -12,6 +12,8 @@
  * 3 but not 2. Using the logic of evaluating subranges like in processData2 I get the correct answer for sample output
  * line2, but not for sample output lines 1 and 3. I'm not sure which evaluation technique you intended to be used so I
  * included functions to process the subranges both ways just in case.
+ *
+ * Updated - The correct function is correctProcessData that will process the data in the desired format.
  */
 
 #include <iostream>
@@ -23,13 +25,15 @@ using namespace std;
 int* getData();
 void processData1(int*);
 void processData2(int*);
+void correctProcessData(int*);
 
 //Main function definition to call various functions associated with calculating upvotes.
 int main()
 {
 	int* upvoteArray = getData();
-	processData1(upvoteArray);
-	processData2(upvoteArray);
+	//processData1(upvoteArray);
+	//processData2(upvoteArray);
+	correctProcessData(upvoteArray);
 }
 
 //Function definition to retrieve the data to be used in calculating upvotes. It will prompt the user to enter
@@ -37,14 +41,14 @@ int main()
 //that many days.
 int* getData()
 {
-	int nValue = 0, kValue = 0, tempValue = 0;
+	int nValue = 0, kValue = 0;
+	unsigned long long int tempValue = 0;
 	int* upvoteArray = NULL;
 	string upvoteData;
 
 	//Capturing the value for N.
 	while(nValue < 1 || nValue > 100000)
 	{
-		cout << "Enter the value for N (1 <= N <= 100,000): ";
 		cin >> nValue;
 	}
 
@@ -53,7 +57,6 @@ int* getData()
 	//Capturing the value for K.
 	while(kValue < 1 || kValue > nValue)
 	{
-		cout << "Enter the value for K (1 <= K <= " << nValue << "): ";
 		cin >> kValue;
 	}
 
@@ -64,14 +67,18 @@ int* getData()
 	//Capturing the values for daily upvote data.
 	for(int i = 2; i < nValue + 2; i++)
 	{
-		do
-		{
-			cout << "Day " << i - 1 << ": ";
-			cin >> tempValue;
-		}
-		while(tempValue > pow(10,9));
+		cin >> tempValue;
 
-		upvoteArray[i] = tempValue;
+		//Reprompting if the tempValue is too large.
+		if(tempValue > pow(10,9))
+		{
+			i--;
+			tempValue = 0;
+		}
+		else
+		{
+			upvoteArray[i] = tempValue;
+		}
 	}
 
 	return upvoteArray;
@@ -160,6 +167,143 @@ void processData2(int* upvoteArray)
 
 		//Outputting the current subrange answer and then resetting the values.
 		cout << "Subrange " << i + 1 << " answer is: " << nonDecreasing - nonIncreasing << endl;
+		nonDecreasing = 0;
+		nonIncreasing = 0;
+	}
+}
+
+//Correct function definition to process the upvote data stored in upvoteArray. This function evaluates subranges by comparing
+//all adjacent values, continuing if the current subrange continues non-decreasing or non-increasing. For example on the subset
+//[1,2,3] it will compare [1,2] and then [1,2,3] and then [2,3] resulting in 3 non-decreasing subranges and 0 non-increasing.
+void correctProcessData(int* upvoteArray)
+{
+	int nonDecreasing = 0, nonIncreasing = 0;
+
+	//For loop to iterate through the array N-K+1 times.
+	for(int i = 0; i < (upvoteArray[0] - upvoteArray[1] + 1); i++)
+	{
+		//For loop to iterate through the subrange K-1 amount of times.
+		for(int j = 0; j < upvoteArray[1] - 1; j++)
+		{
+			//First case, if the first element is smaller than the next then it is non-decreasing.
+			if(upvoteArray[2 + j + i] < upvoteArray[3 + j + i])
+			{
+				nonDecreasing++;
+
+				//For loop to examine the next set of elements to see if they continue to non-decrease or non-increase.
+				for(int k = 1; k <= upvoteArray[1] - 2; k++)
+				{
+					//If the current element is out of range of the window break.
+					if((3 + j + i + k) > (upvoteArray[1] + i + 1))
+					{
+						break;
+					}
+					//If the next element does in fact non-decrease increase nonDecrease.
+					else if(upvoteArray[2 + j + i + k] <= upvoteArray[3 + j + i + k])
+					{
+						nonDecreasing++;
+					}
+					//If not break.
+					else
+					{
+						break;
+					}
+				}
+			}
+			//Second case, if the first element is larger than the next then it is non-increasing.
+			else if(upvoteArray[2 + j + i] > upvoteArray[3 + j + i])
+			{
+				nonIncreasing++;
+
+				//For loop to examine the next set of elements to see if they continue to non-decrease or non-increase.
+				for(int k = 1; k <= upvoteArray[1] - 2; k++)
+				{
+					//If the current element is out of range of the window break.
+					if((3 + j + i + k) > (upvoteArray[1] + i + 1))
+					{
+						break;
+					}
+					//If the next element does in fact non-increase increase nonIncrease.
+					else if(upvoteArray[2 + j + i + k] >= upvoteArray[3 + j + i + k])
+					{
+						nonIncreasing++;
+					}
+					//If not break.
+					else
+					{
+						break;
+					}
+				}
+			}
+			//Third case, if the first element is equal to the second then it is both non-increasing and non-decreasing.
+			else
+			{
+				nonDecreasing++;
+				nonIncreasing++;
+
+				//Boolean flags to test if the chain continues to non-increase or non-decrease.
+				bool nonDecreasingTest = false, nonIncreasingTest = false;
+
+				//For loop to examine the next set of elements to see if they continue to non-decrease or non-increase.
+				for(int k = 1; k <= upvoteArray[1] - 2; k++)
+				{
+					//If the current element is out of range of the window break.
+					if((3 + j + i + k) > (upvoteArray[1] + i + 1))
+					{
+						break;
+					}
+					//First case, if the two elements are equal then check to see if any flags are set to determine what to do next.
+					else if(upvoteArray[2 + j + i + k] == upvoteArray[3 + j + i + k])
+					{
+						//If the chain has been the exact same set of numbers increase both.
+						if(nonIncreasingTest == false && nonDecreasingTest == false)
+						{
+							nonIncreasing++;
+							nonDecreasing++;
+						}
+						//If the chain has already increased then increment nonIncreasing.
+						else if(nonIncreasingTest == true)
+						{
+							nonIncreasing++;
+						}
+						//If the chain has already decreased then increment nonDecreasing.
+						else if(nonDecreasingTest == true)
+						{
+							nonDecreasing++;
+						}
+					}
+					//Second case, if the first element is larger than the next then it is non-increasing.
+					else if(upvoteArray[2 + j + i + k] >= upvoteArray[3 + j + i + k])
+					{
+						//If the chain has already been flagged for nonDecreasing then break.
+						if(nonDecreasingTest == true)
+						{
+							break;
+						}
+						nonIncreasing++;
+						nonIncreasingTest = true;
+					}
+					//Third case, if the first element is smaller than the next then it is non-decreasing.
+					else if(upvoteArray[2 + j + i + k] <= upvoteArray[3 + j + i + k])
+					{
+						//If the chain has already been flagged for nonIncreasing then break.
+						if(nonIncreasingTest == true)
+						{
+							break;
+						}
+						nonDecreasing++;
+						nonDecreasingTest = true;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+		}
+
+		//Outputting the current subrange answer and then resetting the values.
+		cout << nonDecreasing - nonIncreasing << endl;
 		nonDecreasing = 0;
 		nonIncreasing = 0;
 	}
